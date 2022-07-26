b6994ec6e328ca67f9c1ceea967bd67ea1859c97
Markus Timour <markus.timour@vertiseit.se>
2022-07-26 17:54:47 +0200

    Fix: Backspacing entering commands

--- RobotControl.Prompt/PromptUtils.cs
+++ RobotControl.Prompt/PromptUtils.cs
@@ -9,12 +9,28 @@ internal class PromptUtils
         string result = null;
 
         StringBuilder builder = new StringBuilder();
+        var leftCursorPosition = Console.CursorLeft;
 
         ConsoleKeyInfo info = Console.ReadKey(true);
         while (info.Key != ConsoleKey.Enter && info.Key != ConsoleKey.Escape)
         {
-            Console.Write(info.KeyChar);
-            builder.Append(info.KeyChar);
+            if (info.Key == ConsoleKey.Backspace)
+            {
+                if (builder.Length > 0)
+                {
+                    builder.Remove(builder.Length - 1, 1);
+                    Console.SetCursorPosition(leftCursorPosition, Console.CursorTop);
+                    Console.Write(new string(' ', Console.WindowWidth - leftCursorPosition));
+                    Console.SetCursorPosition(leftCursorPosition, Console.CursorTop);
+                    Console.Write(builder.ToString());
+                }
+            }
+            else
+            {
+                Console.Write(info.KeyChar);
+                builder.Append(info.KeyChar);
+            }
+
             info = Console.ReadKey(true);
         }
 
